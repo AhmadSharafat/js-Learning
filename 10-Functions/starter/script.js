aba42decb08a5c576e2435218a22863d243e2e4b
Muhammad Ahmad Sharafat <chaudharyahm12@gmail.com>
2024-09-18 13:19:48 +0500

    Default values For Default Parameters
    Just Learn How to set up default Values for default parameters in functions when we don't recieve values from user !

--- 10-Functions/starter/script.js
+++ 10-Functions/starter/script.js
@@ -1,1 +1,24 @@
 'use strict';
+
+// Default parameters
+
+const bookingDetails = [];
+const flightBookings = function (
+  // This is the ES6 way of writing Default Parameters
+  // This Means when we don't recive response we return default value
+  flightName = 'Ah222',
+  pilotName = 'Al Pacino',
+  numPassengers = 1
+) {
+  //    ES-5 way of writing default parameters
+  //   numPassengers = numPassengers || 1;
+  const booking = {
+    flightName,
+    pilotName,
+    numPassengers,
+  };
+  console.log(booking);
+  bookingDetails.push(booking);
+};
+flightBookings('LH123', 'Al Pacino', 2);
+flightBookings('Ahmad');
