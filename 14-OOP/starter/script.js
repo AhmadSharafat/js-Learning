fdb1f78c8a9bf2918185fb51dc55da65fde333d7
Muhammad Ahmad Sharafat <chaudharyahm12@gmail.com>
2024-10-29 17:08:07 +0500

    Learning About Constructor Functions and New Operator
    I just started learning the OOP and my first concept that I learned is the constructor functions and the new operator Further more, I learn how the constructor method help to create new instances on the base of same properties and with that new operator plays a role in connecting the new object with constructor properties

--- 14-OOP/starter/script.js
+++ 14-OOP/starter/script.js
@@ -1,1 +1,13 @@
 'use strict';
+// Creating objects from the construtor
+const Person = function (firstName, secondName) {
+  //   Defining the properties
+  this.firstName = firstName;
+  this.secondName = secondName;
+};
+// Now creating objects with same properties
+const ahmad = new Person('Ahmad', 'Sharafat');
+console.log(ahmad);
+const sheraz = new Person('Sheraz', 'Sharafat');
+const thomas = new Person('Thomas', 'Shelbey');
+console.log(sheraz, thomas);
