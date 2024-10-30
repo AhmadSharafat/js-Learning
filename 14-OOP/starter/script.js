df9df46c5b6a5dd953956a12bfc80262693c30d3
Muhammad Ahmad Sharafat <chaudharyahm12@gmail.com>
2024-10-30 11:38:53 +0500

    Learn about Prototypal Inheritance

--- 14-OOP/starter/script.js
+++ 14-OOP/starter/script.js
@@ -1,13 +1,22 @@
 'use strict';
 // Creating objects from the construtor
-const Person = function (firstName, secondName) {
+const Person = function (firstName, secondName, birthYear) {
   //   Defining the properties
   this.firstName = firstName;
   this.secondName = secondName;
+  this.birthYear = birthYear;
 };
 // Now creating objects with same properties
-const ahmad = new Person('Ahmad', 'Sharafat');
+const ahmad = new Person('Ahmad', 'Sharafat', 2004);
 console.log(ahmad);
-const sheraz = new Person('Sheraz', 'Sharafat');
-const thomas = new Person('Thomas', 'Shelbey');
+const sheraz = new Person('Sheraz', 'Sharafat', 2009);
+const thomas = new Person('Thomas', 'Shelbey', 1885);
 console.log(sheraz, thomas);
+
+// Prototypes
+Person.prototype.calAge = function () {
+  console.log(2037 - this.birthYear);
+};
+ahmad.calAge();
+sheraz.calAge();
+thomas.calAge();
